2c715908e090204dca50c6f24d8eb3d7f239b055
Nicolas Constanty <nicolas.constanty@epitech.eu>
2018-05-26 12:48:09 +0200

    Add amcharts graph

--- public/js/maps-actinspace.js
+++ public/js/maps-actinspace.js
@@ -7,6 +7,7 @@ var icons = {
         icon: iconBase + 'greenpin.png'
     }
 };
+var building;
 
 let buildingDatas = [];
 let markers = [];
@@ -16,7 +17,8 @@ function getData() {
     req.onreadystatechange = function(event) {
         if (this.readyState === XMLHttpRequest.DONE) {
             if (this.status === 200) {
-                buildingDatas = JSON.parse(this.responseText)["Values"];
+                building = JSON.parse(this.responseText)
+                buildingDatas = building["Values"];
                 initgmap();
             } else {
                 console.log("Status de la réponse: %d (%s)", this.status, this.statusText);
@@ -55,6 +57,10 @@ function initMapi() {
     return map;
 }
 
+function updateGrap() {
+
+}
+
 function initMarker(map)
 {
     let i = 0;
@@ -73,4 +79,129 @@ function initgmap() {
     setInterval(function (map) { updatepos(map) }, 3000);
 }
 
+/**
+ * Create the chart
+ */
+var chart = AmCharts.makeChart( "chartdiv", {
+    "type": "stock",
+    "theme": "light",
+
+    // This will keep the selection at the end across data updates
+    "glueToTheEnd": true,
+
+    // Defining data sets
+    // Panels
+    "panels": [ {
+        "showCategoryAxis": false,
+        "title": "Value",
+        "percentHeight": 60,
+        "stockGraphs": [ {
+            "id": "g1",
+            "valueField": "value",
+            "comparable": true,
+            "compareField": "value"
+        } ],
+        "stockLegend": {}
+    } ],
+
+    // Scrollbar settings
+    "chartScrollbarSettings": {
+        "graph": "g1",
+        "usePeriod": "WW"
+    },
+
+    // Period Selector
+    "periodSelector": {
+        "position": "left",
+        "periods": [ {
+            "period": "DD",
+            "count": 10,
+            "label": "10 days"
+        }, {
+            "period": "MM",
+            "selected": true,
+            "count": 1,
+            "label": "1 month"
+        }, {
+            "period": "YYYY",
+            "count": 1,
+            "label": "1 year"
+        }, {
+            "period": "YTD",
+            "label": "YTD"
+        }, {
+            "period": "MAX",
+            "label": "MAX"
+        } ]
+    },
+
+    // Data Set Selector
+    "dataSetSelector": {
+        "position": "left"
+    },
+
+    // Event listeners
+    "listeners": [ {
+        "event": "rendered",
+        "method": function( event ) {
+            chart.mouseDown = false;
+            chart.containerDiv.onmousedown = function() {
+                chart.mouseDown = true;
+            }
+            chart.containerDiv.onmouseup = function() {
+                chart.mouseDown = false;
+            }
+        }
+    } ]
+} );
+
+var maindate = new Date();
+function generateChartData() {
+    let chartdatasets = [];
+    let i = 0;
+    while(i < buildingDatas.length){
+        chartdatasets.push({value: buildingDatas[i]["Deformation"], date: maindate});
+        let dataset = new AmCharts.DataSet();
+        dataset.title = building["Name"] + " - Point n°" + i;
+        dataset.dataProvider = chartdatasets;
+        chart.dataSets.push(dataset);
+        i = i+1;
+    }
+
+    chart.validateData();
+}
+
+/**
+ * Set up interval to update the data periodically
+ */
+setInterval( function() {
+
+    // if mouse is down, stop all updates
+    if ( chart.mouseDown )
+        return;
+
+    // normally you would load new datapoints here,
+    // but we will just generate some random values
+    // and remove the value from the beginning so that
+    // we get nice sliding graph feeling
+
+    // remove datapoint from the beginning
+    // chartData1.shift();
+    //chartData2.shift();
+    //chartData3.shift();
+    // chartData4.shift();
+
+    // add new datapoint at the end
+    maindate.setDate(maindate.getDate() + 1);
+
+    let i = 0;
+    while(i < buildingDatas.length){
+        chart.dataSets[i].dataProvider({value: buildingDatas[i]["Deformation"], date: maindate});
+        i = i+1;
+    }
+
+    chart.validateData();
+}, 1000 );
+
 getData();
+generateChartData();
